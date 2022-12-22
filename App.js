ac6b1a31c70403ba46bdd5a97651b57448448d19
qiang li <qxl0@yahoo.com>
2022-12-22 16:15:43 -0500

    Tailwind is working

--- App.js
+++ App.js
@@ -5,17 +5,10 @@ import { Text, View } from "react-native";
 export default function App() {
   return (
     <View className="flex-1 items-center justify-center bg-white">
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text className="text-red-500">
+        Open up App.js to start working on your app!
+      </Text>
       <StatusBar style="auto" />
     </View>
   );
 }
-
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     backgroundColor: "#fff",
-//     alignItems: "center",
-//     justifyContent: "center",
-//   },
-// });
