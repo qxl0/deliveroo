3c6590e7d21c5421f849e29504eb0b20af7ed9a4
qiang li <qxl0@yahoo.com>
2022-12-22 16:14:23 -0500

    fixed a couple issues with tailwind

--- App.js
+++ App.js
@@ -1,11 +1,21 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+import { Text, View } from "react-native";
 
 export default function App() {
   return (
-    <View>
+    <View className="flex-1 items-center justify-center bg-white">
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
 }
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: "#fff",
+//     alignItems: "center",
+//     justifyContent: "center",
+//   },
+// });
