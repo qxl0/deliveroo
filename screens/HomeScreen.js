bae74da2cab42237e33c50e98ee0602e8138ff55
qiang li <qxl0@yahoo.com>
2022-12-22 20:35:37 -0500

    Added search bar

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,12 +1,13 @@
-import { View, SafeAreaView, Text, Image } from "react-native";
+import { View, SafeAreaView, Text, Image, TextInput } from "react-native";
 import React, { useLayoutEffect } from "react";
 import { useNavigation } from "@react-navigation/native";
 import {
   UserIcon,
   ChevronDownIcon,
-  SearchIcon,
-  AdjustmentsIcon,
+  MagnifyingGlassIcon,
+  AdjustmentsVerticalIcon,
 } from "react-native-heroicons/outline";
+
 const HomeScreen = () => {
   const navigation = useNavigation();
 
@@ -16,21 +17,33 @@ const HomeScreen = () => {
     });
   });
   return (
-    <SafeAreaView>
+    <SafeAreaView className="bg-white">
       <Text className="text-red-500">
         {/*Header*/}
-        <Image
-          source={{
-            uri: "https://links.papareact.com/wru",
-          }}
-          className="h-7 w-7 bg-gray-300 p-4 rounded-full"
-        />
-        <View className="pl-2">
-          <Text className="font-bold text-gray-400 text-xs">Deliver Now!</Text>
-          <Text className="font-bold text-xl">
-            Current Location
-            <ChevronDownIcon size={20} color="#00CCBB" />
-          </Text>
+        <View className="flex-row items1-center pb-3 mx-4 space-x-2">
+          <Image
+            source={{
+              uri: "https://links.papareact.com/wru",
+            }}
+            className="h-7 w-7 bg-gray-300 p-4 rounded-full"
+          />
+          <View className="flex-1 ">
+            <Text className="font-bold text-gray-400 text-xs">
+              Deliver Now!
+            </Text>
+            <Text className="font-bold text-xl">
+              Current Location
+              <ChevronDownIcon size={20} color="#00CCBB" />
+            </Text>
+          </View>
+          <UserIcon size={35} color="#00CCBB" />
+        </View>
+        <View>
+          <View>
+            <TextInput placeholder="Restaurants and cuisines" />
+            <MagnifyingGlassIcon />
+          </View>
+          <AdjustmentsVerticalIcon />
         </View>
       </Text>
     </SafeAreaView>
