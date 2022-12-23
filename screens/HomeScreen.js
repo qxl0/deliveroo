6eaef1b141483d42c331d7e2122107784f8c9b0e
qiang li <qxl0@yahoo.com>
2022-12-22 19:44:28 -0500

    Added react-native-heroicons

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,11 +1,39 @@
-import { View, Text } from "react-native";
-import React from "react";
-
+import { View, SafeAreaView, Text, Image } from "react-native";
+import React, { useLayoutEffect } from "react";
+import { useNavigation } from "@react-navigation/native";
+import {
+  UserIcon,
+  ChevronDownIcon,
+  SearchIcon,
+  AdjustmentsIcon,
+} from "react-native-heroicons/outline";
 const HomeScreen = () => {
+  const navigation = useNavigation();
+
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerShown: false,
+    });
+  });
   return (
-    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Text>Home Screen</Text>
-    </View>
+    <SafeAreaView>
+      <Text className="text-red-500">
+        {/*Header*/}
+        <Image
+          source={{
+            uri: "https://links.papareact.com/wru",
+          }}
+          className="h-7 w-7 bg-gray-300 p-4 rounded-full"
+        />
+        <View className="pl-2">
+          <Text className="font-bold text-gray-400 text-xs">Deliver Now!</Text>
+          <Text className="font-bold text-xl">
+            Current Location
+            <ChevronDownIcon size={20} color="#00CCBB" />
+          </Text>
+        </View>
+      </Text>
+    </SafeAreaView>
   );
 };
 
