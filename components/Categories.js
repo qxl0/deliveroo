1d7a3cc10581b3d263d1ff498201f39c9c31f90b
qiang li <qxl0@yahoo.com>
2022-12-26 19:42:38 -0500

    Added Image in Card

--- components/Categories.js
+++ components/Categories.js
@@ -13,19 +13,31 @@ const Categories = () => {
       }}
     >
       <CategoryCard
-        imgUrl="https://links.papareact.com/wru"
+        imgUrl="https://image.shutterstock.com/image-photo/assorted-sushi-nigiri-maki-big-260nw-1937661397.jpg"
         title="testing 1"
       />
       <CategoryCard
-        imgUrl="https://links.papareact.com/wru"
+        imgUrl="https://image.shutterstock.com/image-photo/assorted-sushi-nigiri-maki-big-260nw-1937661397.jpg"
         title="testing 2"
       />
       <CategoryCard
-        imgUrl="https://links.papareact.com/wru"
+        imgUrl="https://image.shutterstock.com/image-photo/assorted-sushi-nigiri-maki-big-260nw-1937661397.jpg"
         title="testing 3"
       />
       <CategoryCard
-        imgUrl="https://links.papareact.com/wru"
+        imgUrl="https://image.shutterstock.com/image-photo/assorted-sushi-nigiri-maki-big-260nw-1937661397.jpg"
+        title="testing 4"
+      />
+      <CategoryCard
+        imgUrl="https://image.shutterstock.com/image-photo/assorted-sushi-nigiri-maki-big-260nw-1937661397.jpg"
+        title="testing 4"
+      />
+      <CategoryCard
+        imgUrl="https://image.shutterstock.com/image-photo/assorted-sushi-nigiri-maki-big-260nw-1937661397.jpg"
+        title="testing 4"
+      />
+      <CategoryCard
+        imgUrl="https://image.shutterstock.com/image-photo/assorted-sushi-nigiri-maki-big-260nw-1937661397.jpg"
         title="testing 4"
       />
     </ScrollView>
